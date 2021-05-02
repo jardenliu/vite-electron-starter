181fa3a5242555da815d1d85959756ea146cd98a
jardenliu <15915839707@163.com>
2021-05-02 21:26:32 +0800

    feat: init render files

---
+++ scripts/vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: resolve(__dirname, '../src/renderer'),
+  plugins: [vue()]
+})
